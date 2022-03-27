22f14e529aa051f369cc97651bb079f2df14659b
Grinch Is Me <demyanov0808@gmail.com>
2022-03-28 03:40:31 +0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4944839306b9c800ccc0b2237df9882a.js"
+  "/precache-manifest.8eaa99d9809f90cfa7efeeb844e2e761.js"
 );
 
 workbox.clientsClaim();
