83a7b4f1aebda4cec916fc8fe691ab79cfa15564
Grinch Is Me <demyanov0808@gmail.com>
2022-03-30 16:21:50 +0700

    Updates

---
+++ precache-manifest.83cdd905d74d62c87cbd5995a08fec9b.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "66dd102424e592f1149d",
+    "url": "/ilink/static/css/main.c5d9e379.chunk.css"
+  },
+  {
+    "revision": "66dd102424e592f1149d",
+    "url": "/ilink/static/js/main.66dd1024.chunk.js"
+  },
+  {
+    "revision": "2b447e1be3e7dd9deafc",
+    "url": "/ilink/static/js/1.2b447e1b.chunk.js"
+  },
+  {
+    "revision": "60b17986c9f158945d6f",
+    "url": "/ilink/static/css/2.d9c14aa4.chunk.css"
+  },
+  {
+    "revision": "60b17986c9f158945d6f",
+    "url": "/ilink/static/js/2.60b17986.chunk.js"
+  },
+  {
+    "revision": "9a6f71975b78fe2cfd68",
+    "url": "/ilink/static/js/runtime~main.9a6f7197.js"
+  },
+  {
+    "revision": "bf41f617298e031625feda6039fc1a2e",
+    "url": "/ilink/static/media/2.bf41f617.jpg"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/ilink/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/ilink/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/ilink/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/ilink/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "25c129cb224eed57848447778515da53",
+    "url": "/ilink/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8eaa99d9809f90cfa7efeeb844e2e761.js"
+  "/ilink/precache-manifest.83cdd905d74d62c87cbd5995a08fec9b.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/ilink/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
